fcc6816201b4f268436087e4bce01707ceb5ad3a
Gregor Martynus <gregor@martynus.net>
2014-07-13 14:52:10 +0200

    fix: make /www relative to app, not to plugin
    #10

--- worker.js
+++ worker.js
@@ -3,9 +3,9 @@ var generator = require('./manifest-generator.js');
 
 module.exports = function (hoodie, callback) {
 	// Hack until I can get a path var from hoodie
-	var path = __dirname + '/../../www/';
+	var path = process.env.PWD + '/www/';
 
-	var loaderHTML = 
+	var loaderHTML =
 		'<!DOCTYPE html>' +
 		'<html manifest="/_api/_plugins/appcache/_api/manifest.appcache">' +
 		'<head><meta charset="utf-8" /><title>appCache loader</title></head>' +
@@ -13,7 +13,7 @@ module.exports = function (hoodie, callback) {
 
 	hoodie.config.set('loader', loaderHTML);
 
-	// Omitting an output file path will make the generator only 
+	// Omitting an output file path will make the generator only
 	// pass the resulting text to the callback.
 	var gen = generator(path);
 
