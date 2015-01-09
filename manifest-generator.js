2f3040496437ff1dfc3a284636d54b5c944370d2
Moritz <mochtu@users.noreply.github.com>
2015-01-09 13:29:54 +0100

    Fix indentation of source in manifest-generator

--- manifest-generator.js
+++ manifest-generator.js
@@ -33,7 +33,8 @@ module.exports = function(directory, outFilename) {
 
 			files.forEach(function(element, index, array) {
 				// exclude files on blacklist; include only files (no directories)
-                		if (element.indexOf(outFilename) == -1 && fs.lstatSync(dir + element).isFile()) {					if (element == 'index.html') {
+				if (element.indexOf(outFilename) == -1 && fs.lstatSync(dir + element).isFile()) {
+					if (element == 'index.html') {
 						contents.cache.push('/');
 					} else {
 						contents.cache.push('/' + element);
